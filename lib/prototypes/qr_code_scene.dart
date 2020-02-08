5840c56b1afd2bde25eb591a8dd4a50d42ab39f4
mossnana <samurai.hanso@gmail.com>
2020-02-08 16:37:54 +0700

    Imprement QRCode Scanner to app

---
+++ lib/prototypes/qr_code_scene.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class SelectionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+      child: Text('Pick an option, any option!'),
+    );
+  }
+
+  // A method that launches the SelectionScreen and awaits the
+  // result from Navigator.pop.
+  _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push returns a Future that completes after calling
+    // Navigator.pop on the Selection Screen.
+    final result = await Navigator.push(
+      context,
+      // Create the SelectionScreen in the next step.
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    );
+  }
+}
+
+class SelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Pop here with "Yep"...
+                },
+                child: Text('Yep!'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Pop here with "Nope"
+                },
+                child: Text('Nope.'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/workorder_form_screen.dart
+++ lib/screens/workorder_form_screen.dart
@@ -1,6 +1,8 @@
 
 import 'package:ac_mrp_workorder/models/navigate_arguments.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:odoo_api/odoo_api.dart';
 
 class WorkOrderFormScreen extends StatefulWidget {
@@ -16,33 +18,133 @@ class WorkOrderFormScreen extends StatefulWidget {
 
 class _WorkOrderFormScreen extends State<WorkOrderFormScreen> {
   var moveLines;
+  String barcode = "No code";
+  OdooClient get _userRepository => widget.userRepository;
 
   @override
   void initState() {
     super.initState();
   }
 
-  OdooClient get _userRepository => widget.userRepository;
+
+  Future scan(int id) async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      print("Scanner Code : $barcode, Move Line ID : $id");
+      setState((){
+        this.barcode = barcode;
+      });
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        // The user did not grant the camera permission.
+      } else {
+        // Unknown error.
+      }
+    } on FormatException {
+      // User returned using the "back"-button before scanning anything.
+    } catch (e) {
+      // Unknown error.
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final WorkOrderArgument args = ModalRoute.of(context).settings.arguments;
     final movesList = args.activeMoveLineIds;
 
-    Widget x(index)  => args.activeMoveLineIds.count() != 0 ? Text(args.activeMoveLineIds.datas[index].product.name) : Text('Loading');
-
     Widget _myListView = movesList.count() != 0 ? ListView.builder(
       itemCount: movesList.count(),
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(movesList.datas[index].product.name),
+        return Dismissible(
+          key: Key(movesList.datas[index].id.toString()),
+          background: Container(
+            alignment: AlignmentDirectional.centerEnd,
+            color: Colors.greenAccent,
+            child: Icon(
+              Icons.camera_alt,
+              color: Colors.white,
+            ),
+          ),
+          onDismissed: (direction) {
+            scan(movesList.datas[index].id);
+            setState(() {
+              movesList.datas.removeAt(index);
+            });
+          },
+          direction: DismissDirection.endToStart,
+          child: Card(
+            elevation: 5,
+            child: Container(
+              height: 100.0,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 70.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(5),
+                            topLeft: Radius.circular(5)
+                        ),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage("https://i.imgur.com/TQAOVkUb.jpg")
+                        )
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(movesList.datas[index].product.name),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                            child: Container(
+                              width: 30,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.teal),
+                                  borderRadius: BorderRadius.all(Radius.circular(10))
+                              ),
+                              child: Text("Draft",textAlign: TextAlign.center,),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                            child: Container(
+                              width: 260,
+                              child: Text("Test Test",style: TextStyle(
+                                  fontSize: 15,
+                                  color: Color.fromARGB(255, 48, 48, 54)
+                              ),),
+
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         );
+
+       /* return ListTile(
+          title: Text(movesList.datas[index].product.name),
+        );*/
+
       },
     ) : Text('No Data');
 
     return Scaffold(
+      appBar: new AppBar(
+        title: new Text('Move Line Scanner'),
+      ),
       body: Center(
-        child: _myListView
+        child: _myListView,
       ),
     );
   }
